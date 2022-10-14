d01764d37a79f22957d802aa958e29ee52b5c3f9
Dmither <mr.d.graumann@gmail.com>
2022-10-14 16:00:52 +0300

    change style

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-document.querySelector('.header__burger')
+let burger = document.querySelector('.header__burger');
+let menu = document.querySelector('.menu')
+burger.addEventListener("click", function(event){
+  burger.classList.toggle('header__burger_active');
+  menu.classList.toggle('menu_active');
+})
