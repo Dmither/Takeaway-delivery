a82e03cf1e297b05d2dfed57051bc9a182d701bb
Dmither <mr.d.graumann@gmail.com>
2022-10-19 15:17:25 +0300

    fix menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 let burger = document.querySelector('.header__burger');
-let menu = document.querySelector('.menu')
+let menu = document.querySelector('.menu');
+let body = document.querySelector('body');
+let main = document.querySelector('.main');
 burger.addEventListener("click", function(event){
   burger.classList.toggle('header__burger_active');
   menu.classList.toggle('menu_active');
+  body.classList.toggle('freeze');
+})
+main.addEventListener("click", function(event){
+  if (menu.classList.contains('menu_active')){
+    if (!event.target.closest('.menu')){
+      console.log(event);
+      burger.classList.toggle('header__burger_active');
+      menu.classList.toggle('menu_active');
+      body.classList.toggle('freeze');
+    }
+  }
 })
